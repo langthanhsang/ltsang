9fed3ee2d36bd6f30adf2b3dfea357340df09a06
HP <langthanhsangls@gmail.com>
2022-09-19 20:52:23 +0700

    ltsanfdgdfgdfg

---
+++ source/src/views/product/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="product-list">
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h2 class="display-4"> Product Management</h2>
+      <p><router-link to="/product/create">Add new</router-link></p>
+    </div>
+    <div class="container">
+      <div class="cart-deck mb-3 text-center">
+        <table class="table table-bordered">
+          <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Product name</th>
+            <th scope="col"> Price</th>
+            <th scope="col">Handel</th>
+          </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "List",
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ source/src/views/users/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="users-list">
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h2 class="display-4"> User Management</h2>
+      <p><router-link to="/users/create">Add new</router-link></p>
+    </div>
+    <div class="container">
+      <div class="cart-deck mb-3 text-center">
+        <table class="table table-bordered">
+          <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col"> name</th>
+            <th scope="col"> email</th>
+            <th scope="col">Handel</th>
+          </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "List",
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
